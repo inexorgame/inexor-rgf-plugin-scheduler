4f39aa57c9004252fe12343196f9a9142b19924f
Andreas Schaeffer <andreas@schaeffernet.de>
2022-06-17 17:52:55 +0200

    Code cleanup

--- src/behaviour/entity/timer.rs
+++ src/behaviour/entity/timer.rs
@@ -25,60 +25,6 @@ pub struct Timer {
 
 impl Timer {
     pub fn new<'a>(e: Arc<ReactiveEntityInstance>) -> Result<Timer, BehaviourCreationError> {
-        // // Initial duration
-        // let duration = e.get(TimerProperties::DURATION.as_ref()).and_then(|v| to_duration(&v));
-        // // let duration = e.properties.get(TimerProperties::DURATION.as_ref()).and_then(to_duration);
-        // if duration.is_none() {
-        //     return Err(BehaviourCreationError);
-        // }
-        // let duration = duration.unwrap();
-        //
-        // // Construct timer thread
-        // let (stopper_tx, stopper_rx) = crossbeam::channel::bounded(1);
-        // let (duration_tx, duration_rx) = crossbeam::channel::unbounded();
-        // let entity = e.clone();
-        // let thread_name = format!("{}-{}", e.type_name.clone(), e.id.to_string());
-        // let _handler = task::Builder::new().name(thread_name).spawn(async move {
-        //     let mut duration = duration;
-        //     loop {
-        //         std::thread::sleep(duration);
-        //         entity.set(TimerProperties::TRIGGER.as_ref(), json!(true));
-        //         // Receive duration change
-        //         match duration_rx.try_recv() {
-        //             Ok(d) => {
-        //                 duration = d;
-        //             }
-        //             Err(_) => {}
-        //         }
-        //         // Receive stop event loop
-        //         if stopper_rx.try_recv().is_ok() {
-        //             break;
-        //         }
-        //     }
-        //     debug!("Timer destroyed");
-        // });
-        //
-        // let handle_id = e.properties.get(TimerProperties::DURATION.as_ref()).unwrap().id.as_u128();
-        // e.properties
-        //     .get(TimerProperties::DURATION.as_ref())
-        //     .unwrap()
-        //     .stream
-        //     .read()
-        //     .unwrap()
-        //     .observe_with_handle(
-        //         move |duration| {
-        //             if let Some(duration) = to_duration(duration) {
-        //                 duration_tx.send(duration);
-        //             }
-        //             // if let Some(duration) = duration.as_u64() {
-        //             //     let _ = duration_tx.send(duration);
-        //             // }
-        //             // if let Some(duration) = duration.as_str() {
-        //             //     let _ = duration_tx.send(duration);
-        //             // }
-        //         },
-        //         handle_id,
-        //     );
         Ok(Timer {
             entity: e.clone(),
             job_id: RwLock::new(None),
@@ -91,14 +37,7 @@ impl Timer {
 }
 
 impl Disconnectable for Timer {
-    fn disconnect(&self) {
-        // // Stop event loop thread
-        // let _ = self.stopper_tx.send(());
-        // if let Some(property) = self.entity.properties.get(TimerProperties::DURATION.as_ref()) {
-        //     trace!("Disconnecting {} with id {}", TIMER, self.entity.id);
-        //     property.stream.read().unwrap().remove(self.handle_id);
-        // }
-    }
+    fn disconnect(&self) {}
 }
 
 impl Drop for Timer {
